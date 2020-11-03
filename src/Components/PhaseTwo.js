a49923b0be39a3d83f8feacc8b5e5703377e8dc3
github username <github email address>
2020-11-03 10:01:16 -0500

    Phase two leaderboard complete

--- src/Components/PhaseTwo.js
+++ src/Components/PhaseTwo.js
@@ -3,26 +3,61 @@ import PhaseThree from './PhaseThree'
 
 class PhaseTwo extends React.Component {
 
-// IN PROGRESS
-    testFunction = () => {
-        return this.props.referenceResults.length
+    state = {
+        beatsArray: [],
+        leaderboard: []
     }
 
-        filterSelectedBeats = () => {
-            const winner = this.props.songObj.beats.filter(beat => beat.selected)
+    componentDidMount = () => {
+        fetch("http://localhost:3000/beats")
+        .then(r => r.json())
+        .then(beats =>{ 
+            this.setState({beatsArray: beats})
+        })
+    }
+
+// IN PROGRESS
+
+createBeatsLeaderBoard = () => {
+    const wins = this.state.beatsArray.map(beat => beat.results.filter(result => result.win === true).length)
+    const beatsWithWins = []
+    this.state.beatsArray.forEach(function(v,i){
+        const obj = {};
+        obj.beat = v;
+        obj.wins = wins[i];
+        beatsWithWins.push(obj);
+      });
+      const sortedByWins = beatsWithWins.sort(function (l, r) {
+        return r.wins - l.wins;
+    });
+    // this.setState({leaderboard: sortedByWins})
+    return sortedByWins
+}
+
+
+// COMPLETED
+
+    filterSelectedBeats = () => {
+        const winner = this.props.songObj.beats.filter(beat => beat.selected)
             if (winner){
                 return winner[0].id
             }
-        }
+    }
+
         
         render(){
+            console.log(this.createBeatsLeaderBoard())
             if (this.props.songObj.phase > 2)
                 return(
                     this.props.songObj.beats.length > 0 ? <div><p>Beat ID: {this.filterSelectedBeats()}</p></div> : null
                 )
             else if (this.props.songObj.phase === 2){
                 return (
-                    <p>Leaderboard here</p>
+                <div>
+                    <p>1. {this.createBeatsLeaderBoard()[0] !== undefined ? this.createBeatsLeaderBoard()[0].beat.id : null}</p>
+                    <p>2. {this.createBeatsLeaderBoard()[1] !== undefined ? this.createBeatsLeaderBoard()[1].beat.id : null}</p>
+                    <p>3. {this.createBeatsLeaderBoard()[2] !== undefined ? this.createBeatsLeaderBoard()[2].beat.id : null}</p>
+                </div>
                 )
             }
             else {
